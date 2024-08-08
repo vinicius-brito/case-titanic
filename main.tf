702282ef2f75fdc1ff0d7b606ad6edc70381ccd4
Vinicius Brito <viniciusedutx@gmail.com>
2024-08-08 12:42:20 -0300

    alteração teste captura de id

--- main.tf
+++ main.tf
@@ -273,6 +273,13 @@ resource "aws_api_gateway_resource" "sobreviventes" {
   path_part   = "sobreviventes"
 }
 
+resource "aws_api_gateway_method" "proxy_sobreviventes" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.sobreviventes.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "sobreviventes_get" {
   rest_api_id             = aws_api_gateway_rest_api.my_api.id
   resource_id             = aws_api_gateway_resource.sobreviventes.id
@@ -282,11 +289,26 @@ resource "aws_api_gateway_integration" "sobreviventes_get" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
-resource "aws_api_gateway_method" "proxy_sobreviventes" {
+resource "aws_api_gateway_resource" "survivor_id" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_resource.sobreviventes.id
+  path_part   = "{id}"
+}
+
+resource "aws_api_gateway_method" "get_survivor_by_id" {
   rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.sobreviventes.id
+  resource_id   = aws_api_gateway_resource.survivor_id.id
   http_method   = "GET"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_survivor_by_id" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.survivor_id.id
+  http_method             = aws_api_gateway_method.get_survivor_by_id.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
 }
 
 resource "aws_api_gateway_method_response" "proxy_sobreviventes" {

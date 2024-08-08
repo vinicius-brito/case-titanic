241b5f47868387878ba60146a1e9966238b358e8
Vinicius Brito <viniciusedutx@gmail.com>
2024-08-08 10:43:58 -0300

    nova versão main

--- main.tf
+++ main.tf
@@ -237,7 +237,7 @@ resource "aws_api_gateway_rest_api" "my_api" {
 resource "aws_api_gateway_resource" "root" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   parent_id = aws_api_gateway_rest_api.my_api.root_resource_id
-  path_part = "sobreviventes"
+  path_part = "hello"
 }
 
 resource "aws_api_gateway_method" "proxy" {
@@ -283,4 +283,22 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   stage_name = "dev"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type = "AWS"
+  uri = aws_lambda_function.html_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.html_lambda.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
 }

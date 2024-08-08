50141a5cd45fe373f69e951362a4b089a2e32ef8
Vinicius Brito <viniciusedutx@gmail.com>
2024-08-08 08:25:38 -0300

    teste 1

--- main.tf
+++ main.tf
@@ -104,6 +104,17 @@ resource "aws_iam_role_policy" "lambda_policy" {
   ]
 }
 
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
+  
+  depends_on = [aws_api_gateway_rest_api.my_api]
+}
+
 # --- CloudWatch ---
 
 resource "aws_cloudwatch_log_group" "api" {
@@ -210,4 +221,66 @@ resource "aws_iam_role_policy" "lambda_s3_policy" {
   name   = "lambda_s3_policy"
   role   = aws_iam_role.lambda.id
   policy = data.aws_iam_policy_document.lambda_s3_policy.json
+}
+
+# --- API Gateway ---
+
+resource "aws_api_gateway_rest_api" "my_api" {
+  name = "my-api"
+  description = "My API Gateway"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part = "sobreviventes"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type = "AWS"
+  uri = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
+
+  depends_on = [
+    aws_api_gateway_method.proxy,
+    aws_api_gateway_integration.lambda_integration
+  ]
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.options_integration, # Add this line
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name = "dev"
 }
